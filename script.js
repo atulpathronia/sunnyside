bf960142087997bfa5aee4cf5cc04ec013ac013e
atulpathronia <atulpathronia@gmail.com>
2022-06-24 16:00:32 +0530

    nav

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "use strict";
 
 const button = document.querySelector(".modal");
+const nav = document.querySelector(".main-nav");
+console.log(button);
+console.log(nav);
 
-button.addEventListener("cilck", function () {
-  document.querySelector(".main-nav").classList.toggle("active");
+button.addEventListener("click", function (e) {
+  console.log(e);
+  nav.classList.toggle("active");
 });
+
+// button.addEventListener("cilck", function (e) {
+//   console.log(e);
+//   document.querySelector(".main-nav").classList.toggle("active");
+// });
