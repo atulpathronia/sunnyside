a8646efeb71b734d80581c2126070ef0ebfc0fd5
atulpathronia <atulpathronia@gmail.com>
2022-06-24 15:45:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const button = document.querySelector(".modal");
+
+button.addEventListener("cilck", function () {
+  document.querySelector(".main-nav").classList.toggle("active");
+});
